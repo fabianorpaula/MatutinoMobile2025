181ed75e0efe232a1077b8a281ee397491b873f1
fabianorpaula <fabiano.rpaula@gmail.com>
2025-02-25 09:53:39 -0300

    Mover

--- Assets/Scripts/Personagem.cs
+++ Assets/Scripts/Personagem.cs
@@ -6,6 +6,28 @@ public class Personagem : MonoBehaviour
     public int Diabetes = 0;
 
 
+
+
+    private void Update()
+    {
+        Mover();
+    }
+
+    void Mover()
+    {
+        Vector3 destino = Input.mousePosition;
+
+        Vector3 posTela = Camera.main.ScreenToWorldPoint(destino);
+
+        Vector3 posTelaCorrigida = new Vector3(posTela.x, 
+            posTela.y + 1, 0);
+
+        transform.position = Vector3.MoveTowards(transform.position, 
+            posTelaCorrigida, 0.01f);
+    }
+
+
+
     private void OnCollisionEnter2D(Collision2D colidiu)
     {
         if(colidiu.gameObject.tag == "Doce")
