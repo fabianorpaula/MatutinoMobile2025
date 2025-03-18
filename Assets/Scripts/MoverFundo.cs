6e276e789b48aedf79b3f9cf2a81b5da50a1df83
fabianorpaula <fabiano.rpaula@gmail.com>
2025-03-18 10:20:51 -0300

    TelaVida

--- Assets/Scripts/MoverFundo.cs
+++ Assets/Scripts/MoverFundo.cs
@@ -7,22 +7,17 @@ public class MoverFundo : MonoBehaviour
 
     void Update()
     {
-        Debug.Log(transform.position.y);
+        
         if (transform.position.y < -12)
         {
-            
             transform.position = new Vector3(0, 15, 0);
         }
-
         meuTempo += Time.deltaTime;
-        
         if(meuTempo > 0.01f)
         {
             transform.position = transform.position + 
                 new Vector3(0, -0.01f,0);
             meuTempo = 0;
         }
-
-       
     }
 }
